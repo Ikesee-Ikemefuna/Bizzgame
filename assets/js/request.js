e3e95b49d6de6de0d61ef05186197681d6a69cc7
Ikem Ikenna <97095222+Ikesee-Ikemefuna@users.noreply.github.com>
2025-04-17 18:39:54 +0100

    Update request.js

--- assets/js/request.js
+++ assets/js/request.js
@@ -1,5 +1,5 @@
 const post=async(endpoint, form_data)=>{
-    const fetch_data=await fetch("../../apis/"+endpoint+".php", {
+    const fetch_data=await fetch("https://ikesee-ikemefuna.github.io/Bizzgame/apis/"+endpoint+".php", {
         method:"post",
         body:form_data
     }).catch(err=>alert(err));
@@ -8,7 +8,7 @@ const post=async(endpoint, form_data)=>{
 
 const get=async(endpoint, params=null)=>{
     const url=params?endpoint+".php?"+params:endpoint+".php";
-    const fetch_data=await fetch("../../apis/"+url).catch(err=>alert(err));
+    const fetch_data=await fetch("https://ikesee-ikemefuna.github.io/Bizzgame/apis/"+url).catch(err=>alert(err));
     return await fetch_data.json();
 }
 
