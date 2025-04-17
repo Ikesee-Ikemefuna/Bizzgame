6353dc9396a6d1ffef40441c3a12fb41dba6a983
Ikem Ikenna <97095222+Ikesee-Ikemefuna@users.noreply.github.com>
2025-04-17 19:49:30 +0100

    Update request.js

--- assets/js/request.js
+++ assets/js/request.js
@@ -1,5 +1,5 @@
 const post=async(endpoint, form_data)=>{
-    const fetch_data=await fetch("https://ikesee-ikemefuna.github.io/Bizzgame/apis/"+endpoint+".php", {
+    const fetch_data=await fetch("https://legendary-rotary-phone-7j79p6447rjhx56p-8000.app.github.dev/apis/"+endpoint+".php", {
         method:"post",
         body:form_data
     }).catch(err=>alert(err));
@@ -8,7 +8,7 @@ const post=async(endpoint, form_data)=>{
 
 const get=async(endpoint, params=null)=>{
     const url=params?endpoint+".php?"+params:endpoint+".php";
-    const fetch_data=await fetch("https://ikesee-ikemefuna.github.io/Bizzgame/apis/"+url).catch(err=>alert(err));
+    const fetch_data=await fetch("https://legendary-rotary-phone-7j79p6447rjhx56p-8000.app.github.dev/apis/"+url).catch(err=>alert(err));
     return await fetch_data.json();
 }
 
