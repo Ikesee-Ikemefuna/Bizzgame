205fc6076c5e9382baaa6fc4a585842029d3eea6
Ikem Ikenna <97095222+Ikesee-Ikemefuna@users.noreply.github.com>
2025-04-17 15:55:04 +0100

    Delete app directory

--- app/core/notification/service/sms.py
+++
@@ -1,6 +0,0 @@
-
-
-class SmsNotification:
-    def __init__(self, message: str, receiver: str):
-        self.message = message
-        self.receiver = receiver
--- app/test.py
+++
@@ -1,6 +0,0 @@
-
-from fastapi.testclient import TestClient
-
-from app.app import init_app
-
-client = TestClient(app=init_app())
