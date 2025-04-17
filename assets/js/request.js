d588dc247f651b58af7f98b4722d4b8b43e7e135
Ikem Ikenna <97095222+Ikesee-Ikemefuna@users.noreply.github.com>
2025-04-17 18:33:41 +0100

    Update request.js

--- assets/js/request.js
+++ assets/js/request.js
@@ -1,17 +1,10 @@
 const post=async(endpoint, form_data)=>{
-    const fetch_data=await fetch("https://curly-waffle-q5vqpjgg75jc5jp-8000.app.github.dev/api/v1/"+endpoint, {
+    const fetch_data=await fetch("../../apis/"+endpoint+".php", {
         method:"post",
         body:form_data
     }).catch(err=>alert(err));
     return await fetch_data.json();
 }
-// const post=async(endpoint, form_data)=>{
-//     const fetch_data=await fetch("../../apis/"+endpoint+".php", {
-//         method:"post",
-//         body:form_data
-//     }).catch(err=>alert(err));
-//     return await fetch_data.json();
-// }
 
 const get=async(endpoint, params=null)=>{
     const url=params?endpoint+".php?"+params:endpoint+".php";
