31810bb5ba7279e416cbb864d9c801df452fb1e7
Ikem Ikenna <97095222+Ikesee-Ikemefuna@users.noreply.github.com>
2025-04-18 21:49:26 +0000

    create templates/app.sh create templates/index.html rename main_copy.py as app.py

--- app.py
+++ app.py
@@ -8,21 +8,15 @@ from app.utils.crud.types_crud import response_message
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
-app = init_app()
-fast_api=FastAPI()
+app=FastAPI()
 
 templates=Jinja2Templates(directory="templates");
 
 # app.add_middleware(AuthMiddleware, db_session=session_manager)    
 
-# app.mount("/static", StaticFiles(directory="static"), name="static")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 # app.on_event("startup")
 
-# @app.get('/')
-# async def root():
-#     return JSONResponse(status_code=status.HTTP_200_OK, content = response_message(data="welcome to skill share  project", success_status=True, message="success"))
-
-
-@fast_api.get("/")
+@app.get("/")
 async def index(request:Request):
-   return templates.TemplateResponse("helloworld.jinja", context={"request":request, "id":id})
+   return templates.TemplateResponse("index.html", {"request":request})
---
+++ templates/app.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -e  # Exit on error
+
+# Run the PHP app
+echo "Starting PHP Server"
+php -S localhost:8000
